ffb0588c63fd3fc3d78edbdafb0be18ea725d6fa
Brett Zamir <brettz9@yahoo.com>
2024-09-12 10:54:28 +0800

    initial commit

---
+++ test/unit-test.js
@@ -0,0 +1,8 @@
+import {describe, it} from '../../node_modules/mocha/mocha.js';
+import {expect} from '../../node_modules/chai/chai.js';
+
+describe('My suite', function () {
+  it('my test', function () {
+    expect(true).to.equal(true);
+  });
+});
