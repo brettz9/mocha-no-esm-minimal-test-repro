1d41d0072343057eae29a3047c6fd4c33e23caea
Brett Zamir <brettz9@yahoo.com>
2025-12-19 02:49:07 -0700

    complete testing

--- test/unit-test.js
+++ test/unit-test.js
@@ -1,8 +1,45 @@
-import {describe, it} from '../node_modules/mocha/mocha.mjs';
+import {
+  describe, it,
+  after, afterEach, before, beforeEach,
+  context, specify,
+  xdescribe, xit,
+  xcontext, xspecify,
+} from '../node_modules/mocha/mocha.mjs';
 import {expect} from '../node_modules/chai/chai.js';
 
 describe('My suite', function () {
+  after(() => {
+    console.log('after');
+  });
+  afterEach(() => {
+    console.log('after each');
+  });
+  before(() => {
+    console.log('before');
+  });
+  beforeEach(() => {
+    console.log('before each');
+  });
+
   it('my test', function () {
+    expect(true).to.equal(true);
+  });
+});
+
+context('My suite (context)', function () {
+  specify('my test (specify)', function () {
+    expect(true).to.equal(true);
+  });
+});
+
+xdescribe('My suite', function () {
+  xit('my test', function () {
+    expect(true).to.equal(true);
+  });
+});
+
+xcontext('My suite (context)', function () {
+  xspecify('my test (specify)', function () {
     expect(true).to.equal(true);
   });
 });
